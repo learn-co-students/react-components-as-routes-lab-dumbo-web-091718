36164885268a31641793916900034e36b3470607
shemarjoseph <shemarjoseph@yahoo.com>
2018-12-01 20:31:56 -0500

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,28 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {
+        movies.map(movie => {
+          return (
+            <div>
+              <div>{movie.title}</div>
+              <div>{movie.time}</div>
+              <div>
+              {
+                <ul>
+                  {
+                    movie.genres.map(genre => {
+                      return <li>{genre}</li>
+                    })
+                  }
+                </ul>
+              }
+              </div>
+            </div>
+          )
+        })
+      }
     </div>
   );
 };
