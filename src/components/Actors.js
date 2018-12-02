73353e45a0b66ff6e80fe830c1b2789076ceaf45
Christine Gomez <christine.gomez1226@gmail.com>
2018-12-02 12:23:48 -0500

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,8 +4,19 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
-    </div>
+    <h1>Actors Page</h1>
+    {actors.map((actor, idx) => (
+      <div>
+        <h2>Name: {actor.name}</h2>
+        <p>Movies:</p>
+        <ul>
+          {actor.movies.map((movie, idx) => (
+            <li>{movie}</li>
+          ))}
+        </ul>
+      </div>
+    ))}
+  </div>
   );
 };
 
