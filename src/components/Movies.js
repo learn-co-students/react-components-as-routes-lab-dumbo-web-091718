cdcb7578b2b5904c2a934c5875db8095f9fc016d
Bijon Miles <bijonmiles01@gmail.com>
2019-03-23 17:25:53 -0400

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -5,6 +5,19 @@ const Movies = () => {
   return (
     <div>
         {/*{code here}*/}
+        <h1> Movie Page </h1>
+        {movies.map((movie, index) => (
+          <div key={index}>  <h3> Name: {movie.title} </h3>
+            <p> Movies:  </p>
+             <ul>
+                {movie.genres.map((genre, index) => (
+                  <li key={index}> {genre} </li>
+                ))}
+             </ul>
+
+
+          </div>
+        ))}
     </div>
   );
 };
